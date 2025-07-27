491a6cfd34bd83aa5bf528e73f177dc2746fc0b8
Dennis Yeom <yeom.dennis@gmail.com>
2025-07-27 19:19:14 -0400

    first commit! hello, world!

---
+++ hello.go
@@ -0,0 +1,11 @@
+// package declaration.
+// main package contains code to start the program.
+package main
+
+//packages references in this file.
+//oddity of go, we have to import whole package not individual functions.
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello, world!\n!")
+}
